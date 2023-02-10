fcbf4d2836018796a2251ec9fcddd43a71c957f8
Amer Khanfar <abk.sg8@gmail.com>
2023-02-10 17:39:52 +0400

    done

---
+++ data.js
@@ -0,0 +1,28 @@
+export const buttons = [
+  {
+    id: 1,
+    color: "#27224e",
+    width: "28vw",
+  },
+  {
+    id: 2,
+    color: "#e85e0c",
+    width: "30vw",
+  },
+  {
+    id: 3,
+    color: "#384595",
+    width: "28vw",
+    center: "center",
+  },
+  {
+    id: 4,
+    color: "#5fb77e",
+    width: "30vw",
+  },
+  {
+    id: 5,
+    color: "#e73264",
+    width: "30vw",
+  },
+];
